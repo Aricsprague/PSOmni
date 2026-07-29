404ad6fd27299c6ce0dcc81d72a9f4e53fcaf689
arics <arics@APHELION>
2026-07-28 20:04:20 -0500

    Add project files.

---
+++ PSOmni/MainForm.cs
@@ -0,0 +1,15 @@
+namespace PSOmni
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
